9f0ce7ae9db5142752a21c1c2c147931eb63eb8e
Mark Otwane <128910458+MarkOtwane@users.noreply.github.com>
2024-02-27 00:30:38 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,4 +1,4 @@
 name = input("Enter your name:\n")
 age = int(input("Enter your age:\n"))
 location = input("Enter your location:\n")
-print(f"Hello {name}, I'm {age} years old and I live in {location}")
+print(f"Hello {name}, you are {age} years old and you live in {location}")
